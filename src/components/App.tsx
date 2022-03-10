a3a857a17b91570ea30fbda204893719025e1747
Stanislav <blackfur4a@mail.ru>
2022-03-10 12:52:19 +0300

    feat: initial project

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+
+export const App = () => <ChakraProvider theme={theme}></ChakraProvider>;
